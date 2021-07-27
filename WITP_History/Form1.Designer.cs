10380fa8a776a11e5bc4f72afb3d1536cab9a54b
Dozed12 <up201607929@fe.up.pt>
2021-07-27 23:08:47 +0100

    Mostly complete

--- WITP_History/Form1.Designer.cs
+++ WITP_History/Form1.Designer.cs
@@ -28,13 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1276, 735);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "History Search";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Japan",
+            "Allies"});
+            this.comboBox1.Location = new System.Drawing.Point(64, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "Japan";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(27, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Side:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBox1.Font = new System.Drawing.Font("Roboto", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.richTextBox1.Location = new System.Drawing.Point(22, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1232, 650);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(204, 20);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1050, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(154, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Search:";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.DimGray;
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1276, 735);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Settings";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(261, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(753, 40);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Archive folder is: War in the Pacific Admiral Edition\\SAVE\\archive\r\nIf you don\'t " +
+    "have an archive folder you have to enable it for your game shortcut using -archi" +
+    "ve";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(26, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(158, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(1109, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Save Archive Path:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1284, 761);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1284, 761);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "War in the Pacific Archive Searcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- WITP_History/Form1.cs
+++ WITP_History/Form1.cs
@@ -6,15 +6,323 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace WITP_History
 {
     public partial class Form1 : Form
     {
+
+        public string archivePath;
+
+        //History <Date, Events>
+        public Dictionary<string, Tuple<List<string>, List<string>>> japan;
+        public Dictionary<string, Tuple<List<string>, List<string>>> allies;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            string path = "INVALID";
+            if (result == DialogResult.OK)
+            {
+                path = folderBrowserDialog1.SelectedPath;
+                
+            }
+            if (result == DialogResult.Cancel)
+            {
+                return;
+
+            }
+            Console.WriteLine(path);
+
+            //Save new path
+            File.WriteAllText("WITP_History_Settings.txt", path);
+            archivePath = path;
+
+            DisplayPath();
+
+            LoadHistory();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Read Archive Path
+            if (File.Exists("WITP_History_Settings.txt"))
+            {
+                archivePath = File.ReadAllText("WITP_History_Settings.txt");
+                DisplayPath();
+                LoadHistory();
+            }
+        }
+
+        private void DisplayPath()
+        {
+            textBox1.Text = archivePath;
+        }
+
+        private void LoadHistory()
+        {
+            string[] files = Directory.GetFiles(archivePath);
+
+            japan = new Dictionary<string, Tuple<List<string>, List<string>>>();
+            allies = new Dictionary<string, Tuple<List<string>, List<string>>>();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                //Japan Operations
+                if (files[i].Contains("joperationsreport_"))
+                {
+                    string[] lines = File.ReadAllLines(files[i]);
+
+                    string date = lines[0];
+                    date = date.Replace("OPERATIONAL REPORT FOR ", "");
+                    date = date.Replace("\n", "");
+
+                    List<string> events = lines.Skip<string>(2).ToArray<string>().ToList<string>();
+                    List<string> eventsLower = events.ConvertAll(d => d.ToLower());
+
+                    for (int j = 0; j < events.Count; j++)
+                    {
+                        events[j] = "\t" + events[j];
+                    }
+
+                    if (japan.ContainsKey(date))
+                    {
+                        japan[date].Item1.AddRange(events);
+                        japan[date].Item2.AddRange(eventsLower);
+                    }
+                    else
+                        japan.Add(date, new Tuple<List<string>, List<string>>(events, eventsLower));
+                }
+
+                //Japan SigInt
+                if (files[i].Contains("jsigint_"))
+                {
+                    string[] lines = File.ReadAllLines(files[i]);
+
+                    string date = lines[0];
+                    date = date.Replace("SIG INT REPORT FOR ", "");
+                    date = date.Replace("\n", "");
+
+                    List<string> events = lines.Skip<string>(2).ToArray<string>().ToList<string>();
+                    List<string> eventsLower = events.ConvertAll(d => d.ToLower());
+
+                    for (int j = 0; j < events.Count; j++)
+                    {
+                        events[j] = "\t" + events[j];
+                    }
+
+                    if (japan.ContainsKey(date))
+                    {
+                        japan[date].Item1.AddRange(events);
+                        japan[date].Item2.AddRange(eventsLower);
+                    }
+                    else
+                        japan.Add(date, new Tuple<List<string>, List<string>>(events, eventsLower));
+                }
+
+                //Allies Operations
+                if (files[i].Contains("aoperationsreport_"))
+                {
+                    string[] lines = File.ReadAllLines(files[i]);
+
+                    string date = lines[0];
+                    date = date.Replace("OPERATIONAL REPORT FOR ", "");
+                    date = date.Replace("\n", "");
+
+                    List<string> events = lines.Skip<string>(2).ToArray<string>().ToList<string>();
+                    List<string> eventsLower = events.ConvertAll(d => d.ToLower());
+
+                    for (int j = 0; j < events.Count; j++)
+                    {
+                        events[j] = "\t" + events[j];
+                    }
+
+                    if (allies.ContainsKey(date))
+                    {
+                        allies[date].Item1.AddRange(events);
+                        allies[date].Item2.AddRange(eventsLower);
+                    }
+                    else
+                        allies.Add(date, new Tuple<List<string>, List<string>>(events, eventsLower));
+                }
+
+                //Allies SigInt
+                if (files[i].Contains("asigint_"))
+                {
+                    string[] lines = File.ReadAllLines(files[i]);
+
+                    string date = lines[0];
+                    date = date.Replace("SIG INT REPORT FOR ", "");
+                    date = date.Replace("\n", "");
+
+                    List<string> events = lines.Skip<string>(2).ToArray<string>().ToList<string>();
+                    List<string> eventsLower = events.ConvertAll(d => d.ToLower());
+
+                    for (int j = 0; j < events.Count; j++)
+                    {
+                        events[j] = "\t" + events[j];
+                    }
+
+                    if (allies.ContainsKey(date))
+                    {
+                        allies[date].Item1.AddRange(events);
+                        allies[date].Item2.AddRange(eventsLower);
+                    }
+                    else
+                        allies.Add(date, new Tuple<List<string>, List<string>>(events, eventsLower));
+                }
+
+                //Combat Reports (Shared)
+                if (files[i].Contains("combatreport_"))
+                {
+                    string[] lines = File.ReadAllLines(files[i]);
+
+                    string date = lines[0];
+                    date = date.Replace("AFTER ACTION REPORTS FOR ", "");
+                    date = date.Replace("\n", "");
+
+                    string[] events = lines.Skip<string>(1).ToArray<string>();
+
+                    //Combine Combat Report
+                    List<string> reports = new List<string>();
+                    string reportLine = "";
+                    for (int j = 0; j < events.Length; j++)
+                    {
+                        if(events[j].Contains("---"))
+                        {
+                            reports.Add(reportLine);
+                            reportLine = "";
+                        }
+                        else
+                        {
+                            if (events[j] == "" || events[j] == " " || events[j] == "\n")
+                                continue;
+
+                            if (reportLine == "")
+                                reportLine += "\t" + events[j] + "\n";
+                            else
+                                reportLine += "\t\t" + events[j] + "\n";
+                        }
+                    }
+
+                    //Only add for each side if contains "Japanese" or "Allied" (Fog of War)
+                    for (int j = 0; j < reports.Count; j++)
+                    {
+                        if(reports[j].Contains("Japanese"))
+                        {
+                            if (japan.ContainsKey(date))
+                            {
+                                japan[date].Item1.Add(reports[j]);
+                                japan[date].Item2.Add(reports[j].ToLower());
+                            }
+                            else
+                                japan.Add(date, new Tuple<List<string>, List<string>>(new List<string>() { reports[j] }, new List<string>() { reports[j].ToLower() }));
+                        }
+                    }
+
+                    for (int j = 0; j < reports.Count; j++)
+                    {
+                        if (reports[j].Contains("Allied"))
+                        {
+                            if (allies.ContainsKey(date))
+                            {
+                                allies[date].Item1.Add(reports[j]);
+                                allies[date].Item2.Add(reports[j].ToLower());
+                            }
+                            else
+                                allies.Add(date, new Tuple<List<string>, List<string>>(new List<string>() { reports[j] }, new List<string>() { reports[j].ToLower() }));
+                        }
+                    }
+                }
+            }
+
+            richTextBox1.Text = "";
+
+            // BENCHMARK
+            if (false)
+            {
+                DateTime start = DateTime.Now;
+                for (int i = 0; i < 10000; i++)
+                {
+                    Query("Kota");
+                }
+                DateTime end = DateTime.Now;
+                Console.WriteLine("Time: " + (end - start));
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = Query(textBox2.Text.ToLower());
+        }
+
+        private string Query(string query)
+        {
+            if (japan == null || allies == null)
+                return "Set the path to your Archives folder in the Settings tab";
+
+            //Dont search unless some characters
+            if (query.Replace(" ", "").Length <= 2)
+                return "Search query too small";
+
+            string queryResults = "";
+
+            //Japan or Allies
+            Dictionary<string, Tuple<List<string>, List<string>>> database = null;
+            if (comboBox1.SelectedIndex == 0)
+                database = japan;
+            else
+                database = allies;
+
+            //Match
+            List<string> results = new List<string>();
+            foreach (var item in database)
+            {
+                List<string> events = database[item.Key].Item1;
+                List<string> eventsLowered = database[item.Key].Item2;
+
+                results.Clear();
+
+                //Get matches
+                for (int i = 0; i < eventsLowered.Count; i++)
+                {
+                    if (eventsLowered[i].Contains(query))
+                    {
+                        results.Add(events[i]);
+                    }
+                }
+
+                //Display matches
+                if (results.Count > 0)
+                {
+                    queryResults += "\n" + item.Key + "\n";
+
+                    for (int i = 0; i < results.Count; i++)
+                    {
+                        queryResults += results[i] + "\n";
+                    }
+                }
+            }
+
+            return queryResults;
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            textBox2_TextChanged(sender, e);
         }
     }
 }
